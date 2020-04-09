cd0f4d5138a0d891dd37d669c3e5b1470e8ec34c
Legaiao <52788184+Legaiao@users.noreply.github.com>
2020-04-09 13:38:52 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'crudFirebase';
+  items: Observable<any[]>;
+  constructor(private db: AngularFirestore) {
+  this.items = db.collection('users').valueChanges();
+     }
+  }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB9EPJUPFYPVCkbzxm2eY_-JvlVmTmAOrY",
+    authDomain: "my-games-aa96f.firebaseapp.com",
+    databaseURL: "https://my-games-aa96f.firebaseio.com",
+    projectId: "my-games-aa96f",
+    storageBucket: "my-games-aa96f.appspot.com",
+    messagingSenderId: "701125325693",
+    appId: "1:701125325693:web:0dd43e9bc3a06f28b31214"
+  }
+};
